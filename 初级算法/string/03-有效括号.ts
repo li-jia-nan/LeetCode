24e06bf17d6990fdca5ed39e5d770b53de538768
栗嘉男 <574980606@qq.com>
2022-02-24 23:33:48 +0800

    c

---
+++ 初级算法/string/03-有效括号.ts
@@ -0,0 +1,24 @@
+export const isValid = (s: string): boolean => {
+  if (s.length % 2 !== 0) {
+    return false;
+  }
+  const stack = [];
+  const map = new Map();
+  map.set('(', ')');
+  map.set('{', '}');
+  map.set('[', ']');
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i];
+    if (map.has(c)) {
+      stack.push(c);
+    } else {
+      const t = stack[stack.length - 1];
+      if (map.get(t) === c) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
