9691d2698f1273682afff37e0445bc3ff4f21266
栗嘉男 <574980606@qq.com>
2022-03-06 00:56:20 +0800

    add

--- 初级算法/number/04-加一.ts
+++ 初级算法/number/04-加一.ts
@@ -1,3 +1,21 @@
 export const plusOne = (digits: number[]): number[] => {
   return [...(BigInt(digits.join('')) + BigInt(1)).toString()].map(Number);
 };
+
+const list = [1, 1, 1, 1, 1, 1];
+
+const flag1 = list.every((n, i) => (i === list.length - 1 ? n : list[i + 1] <= n));
+
+const flag2 = list.every((n, i) => (i === list.length - 1 ? n : list[i + 1] >= n));
+
+if (flag1) {
+  console.log('单调递减数组');
+}
+
+if (flag2) {
+  console.log('单调递增数组');
+}
+
+if (!flag1 && !flag2) {
+  console.log('不是递增也不是递减');
+}
