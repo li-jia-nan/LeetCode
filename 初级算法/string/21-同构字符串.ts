b5509a80a95f6f662ee092d754d328f7293a8c98
栗嘉男 <574980606@qq.com>
2022-05-26 21:43:54 +0800

    fix

--- 初级算法/string/21-同构字符串.ts
+++ 初级算法/string/21-同构字符串.ts
@@ -1,3 +1,6 @@
 export const isIsomorphic = (s: string, t: string): boolean => {
+  if (s.length !== t.length) {
+    return false;
+  }
   return [...s].every((_, i) => s.indexOf(s[i]) === t.indexOf(t[i]));
 };
